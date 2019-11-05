871e1fa30d6504435ff93cf899a2b19cabf68203
shigz97 <shigz97@gmail.com>
2019-11-05 19:04:03 +0800

    增加异步操作

--- io/client.go
+++ io/client.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+	"time"
 )
 
 func request(str string) {
-	conn, err := net.Dial("tcp", "192.168.3.72:9999")
+	conn, err := net.Dial("tcp", "localhost:8888")
 	if err != nil {
 		fmt.Println("error in dialing, ", err.Error())
 	}
@@ -17,18 +18,42 @@ func request(str string) {
 		fmt.Println("error in writing, ", err.Error())
 		return
 	}
+	reschan := getResponse(conn)
+	go func() {
+		for res := range reschan {
+			fmt.Println(res)
+			break
+		}
+	}()
+	fmt.Println(str)
+	//time.Sleep(1 * time.Second)
+	/*for res := range reschan {
+		fmt.Println(res)
+		break
+	}*/
+
+}
+
+func getResponse(conn net.Conn) chan string {
 	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("err reading, ", err.Error())
-	}
-	fmt.Println(string(buf[:n]))
+	res := make(chan string)
+	go func() {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("err reading, ", err.Error())
+		}
+		//fmt.Println(string(buf[:n]))
+		res <- string(buf[:n])
+		time.Sleep(time.Second * 1)
+	}()
+	return res
 }
 
 func main() {
 
-	for i := 0; i < 500; i++ {
+	for i := 0; i < 50; i++ {
 		request("hello" + strconv.Itoa(i))
+		//time.Sleep(time.Second * 2)
 	}
-	//<-time.After(time.Second * 10)
+	<-time.After(time.Second * 20)
 }
